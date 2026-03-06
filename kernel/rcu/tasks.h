0fecc24a0568ebe6ccc50e972761998abff62252
mdx <dgmico@>
2026-03-07 00:40:13 +0800

    Fix: 修复 call_rcu_tasks_trace 重定义并启用 TASKS_TRACE_RCU

--- kernel/rcu/tasks.h
+++ kernel/rcu/tasks.h
@@ -418,10 +418,3 @@ void rcu_read_unlock_trace_special(struct task_struct *t)
 }
 EXPORT_SYMBOL_GPL(rcu_read_unlock_trace_special);
 #endif /* #ifdef CONFIG_TASKS_TRACE_RCU */
-
-#ifndef CONFIG_TASKS_TRACE_RCU
-static inline void call_rcu_tasks_trace(struct rcu_head *rhp, rcu_callback_t func)
-{
-	call_rcu(rhp, func);
-}
-#endif
