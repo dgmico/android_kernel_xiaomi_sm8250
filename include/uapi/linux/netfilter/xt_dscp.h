5a236ec9257553a322f7c504f606a408a7184ccf
mdx <dgmico@>
2026-03-09 16:13:36 +0800

    Fix: 彻底解决 Netfilter 编译错误及 Recovery 刷入失败
    - 通过合并冲突的头文件内容，解决 macOS 大小写不敏感导致的文件定义丢失问题
    - 保持 AnyKernel3 打包逻辑优化，确保刷入时能找到 update-binary

--- include/uapi/linux/netfilter/xt_dscp.h
+++ include/uapi/linux/netfilter/xt_dscp.h
@@ -25,3 +25,21 @@ struct xt_tos_target_info {
 };
 
 #endif /* _XT_DSCP_TARGET_H */
+#ifndef _XT_DSCP_H
+#define _XT_DSCP_H
+#include <linux/types.h>
+#define XT_DSCP_MASK	0xfc
+#define XT_DSCP_SHIFT	2
+#define XT_DSCP_MAX	0x3f
+struct xt_DSCP_info {
+	__u8 dscp;
+};
+struct xt_tos_target_info {
+	__u8 tos_value;
+	__u8 tos_mask;
+};
+struct xt_dscp_info {
+	__u8 dscp;
+	__u8 invert;
+};
+#endif
--- include/uapi/linux/netfilter/xt_rateest.h
+++ include/uapi/linux/netfilter/xt_rateest.h
@@ -15,3 +15,27 @@ struct xt_rateest_target_info {
 };
 
 #endif /* _XT_RATEEST_TARGET_H */
+#ifndef _XT_RATEEST_H
+#define _XT_RATEEST_H
+#include <linux/types.h>
+#ifndef IFNAMSIZ
+#define IFNAMSIZ 16
+#endif
+struct xt_rateest;
+struct xt_rateest_target_info {
+	char		name[IFNAMSIZ];
+	__u8		interval;
+	__u8		ewma_log;
+	struct xt_rateest	*est __attribute__((aligned(8)));
+};
+struct xt_rateest_match_info {
+	char		name1[IFNAMSIZ];
+	char		name2[IFNAMSIZ];
+	__u16		flags;
+	__u16		mode;
+	__u32		bps1, pps1;
+	__u32		bps2, pps2;
+	struct xt_rateest *est1 __attribute__((aligned(8)));
+	struct xt_rateest *est2 __attribute__((aligned(8)));
+};
+#endif
