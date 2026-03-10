08bbf1f5102b12cdd07e005a014a912427a3d7b8
mdx <dgmico@>
2026-03-10 09:03:16 +0800

    Fix: 修复 macOS 上 Netfilter UAPI 头文件的大小写敏感冲突
    - 确保大小写不同的头文件在 Git 索引中指向完全相同的包含修复内容的 blob
    - 解决了 macOS 大小写不敏感环境导致的 modified 状态无法清除的问题

--- include/uapi/linux/netfilter/xt_DSCP.h
+++ include/uapi/linux/netfilter/xt_DSCP.h
@@ -1,18 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 #ifndef _XT_DSCP_H
 #define _XT_DSCP_H
+
 #include <linux/types.h>
-#define XT_DSCP_MASK	0xfc
-#define XT_DSCP_SHIFT	2
-#define XT_DSCP_MAX	0x3f
+
+#define XT_DSCP_MASK    0xfc
+#define XT_DSCP_SHIFT   2
+#define XT_DSCP_MAX     0x3f
+
+/* target info */
 struct xt_DSCP_info {
 	__u8 dscp;
 };
+
 struct xt_tos_target_info {
 	__u8 tos_value;
 	__u8 tos_mask;
 };
+
+/* match info */
 struct xt_dscp_info {
 	__u8 dscp;
 	__u8 invert;
 };
-#endif
+
+#endif /* _XT_DSCP_H */
--- include/uapi/linux/netfilter/xt_RATEEST.h
+++ include/uapi/linux/netfilter/xt_RATEEST.h
@@ -1,16 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 #ifndef _XT_RATEEST_H
 #define _XT_RATEEST_H
+
 #include <linux/types.h>
+#include <linux/if.h>
+
 #ifndef IFNAMSIZ
 #define IFNAMSIZ 16
 #endif
+
 struct xt_rateest;
+
 struct xt_rateest_target_info {
-	char		name[IFNAMSIZ];
-	__u8		interval;
+	char			name[IFNAMSIZ];
+	__s8			interval;
 	__u8		ewma_log;
+
+	/* Used internally by the kernel */
 	struct xt_rateest	*est __attribute__((aligned(8)));
 };
+
 struct xt_rateest_match_info {
 	char		name1[IFNAMSIZ];
 	char		name2[IFNAMSIZ];
@@ -21,4 +30,5 @@ struct xt_rateest_match_info {
 	struct xt_rateest *est1 __attribute__((aligned(8)));
 	struct xt_rateest *est2 __attribute__((aligned(8)));
 };
-#endif
+
+#endif /* _XT_RATEEST_H */
