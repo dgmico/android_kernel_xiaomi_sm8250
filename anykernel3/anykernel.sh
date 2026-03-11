530c6d3908d49f43487a224e00ef250456fc22df
mdx <dgmico15@gmail.com>
2026-03-11 15:40:21 +0000

    Test: enhance minimal build with AVB patch and DTB

--- anykernel3/anykernel.sh
+++ anykernel3/anykernel.sh
@@ -16,9 +16,9 @@ supported.patchlevels=
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
+is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=0;
+patch_vbmeta_flag=1;
 
 ## AnyKernel methods (pre-built tools/busybox)
 # import for functions
